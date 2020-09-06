c4df47db4c2da3e9d2f405c74676c6503d9ddbb2
Zagg2732 <Zagg2732@gmail.com>
2020-09-07 00:07:04 +0900

    step3 구현

--- src/BlackjackHand.java
+++ src/BlackjackHand.java
@@ -1,3 +1,28 @@
 public class BlackjackHand extends Deck{
-    //step3 미구현
+
+    public int getValue() {
+        int value = 0;
+        int aceCount = 0;
+
+        for (Card card : getCards()) { //for-each 문으로 핸드에 있는 카드목록 순회
+            BlackjackCard bjCard = (BlackjackCard) card; //BlackjackCard로 캐스팅
+            value += bjCard.getValue();
+            if (bjCard.isAce()) {  // Ace면 aceCount를 1증가
+                aceCount++;
+            }
+        }
+        while (aceCount > 0 && value > 21) {  // value가 21이상인데 ace가 있으면 규칙에따라 ace는 11점에서 1점이됨 (-10점)
+            value -= 10;
+            aceCount--;
+        }
+        return value;
+    }
+
+    public boolean isBusted() {  // value가 21보다 클경우 파산
+        return getValue() > 21;
+    }
+
+    public boolean isBlackjack() { // value가 두장으로 21이면 블랙잭
+        return getValue() == 21 && getCards().size() == 2;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,55 @@
 public class Main {
     public static void main(String[] args) {
 
-        //step2 구현 테스트 코드
+        //step3 테스트코드
+
+        BlackjackHand hand1 = new BlackjackHand();
+        hand1.addCard(new BlackjackCard(3, 1));
+        hand1.addCard(new BlackjackCard(4, 11));
+        System.out.println(hand1.getValue());
+        System.out.println(hand1.isBlackjack());
+        System.out.println(hand1.isBusted());
+
+        BlackjackHand hand2 = new BlackjackHand();
+        hand2.addCard(new BlackjackCard(1, 4));
+        hand2.addCard(new BlackjackCard(2, 8));
+        hand2.addCard(new BlackjackCard(2, 9));
+        System.out.println(hand2.getValue());
+        System.out.println(hand2.isBlackjack());
+        System.out.println(hand2.isBusted());
+
+        BlackjackHand hand3 = new BlackjackHand();
+        hand3.addCard(new BlackjackCard(2, 5));
+        hand3.addCard(new BlackjackCard(4, 9));
+        hand3.addCard(new BlackjackCard(2, 13));
+        System.out.println(hand3.getValue());
+        System.out.println(hand3.isBlackjack());
+        System.out.println(hand3.isBusted());
+
+        BlackjackHand hand4 = new BlackjackHand();
+        hand4.addCard(new BlackjackCard(2, 8));
+        hand4.addCard(new BlackjackCard(1, 1));
+        System.out.println(hand4.getValue());
+        System.out.println(hand4.isBlackjack());
+        System.out.println(hand4.isBusted());
+
+        BlackjackHand hand5 = new BlackjackHand();
+        hand5.addCard(new BlackjackCard(1, 7));
+        hand5.addCard(new BlackjackCard(1, 9));
+        hand5.addCard(new BlackjackCard(3, 1));
+        System.out.println(hand5.getValue());
+        System.out.println(hand5.isBlackjack());
+        System.out.println(hand5.isBusted());
+
+        BlackjackHand hand6 = new BlackjackHand();
+        hand6.addCard(new BlackjackCard(2, 1));
+        hand6.addCard(new BlackjackCard(1, 1));
+        System.out.println(hand6.getValue());
+        System.out.println(hand6.isBlackjack());
+        System.out.println(hand6.isBusted());
+
+        /*
+        step2 구현 테스트 코드
         BlackjackCard card1 = new BlackjackCard(1, 1);
         BlackjackCard card2 = new BlackjackCard(2, 6);
         BlackjackCard card3 = new BlackjackCard(4, 12);
@@ -13,7 +61,10 @@ public class Main {
         System.out.println(card3.getValue());
         System.out.println(card3.isAce());
 
-        /*//52장 스탠다드 카드 덱 생성
+        */
+
+        /*
+        //52장 스탠다드 카드 덱 생성
         Deck standardDeck = new Deck();
 
         for (int suit = 1; suit <= 4; suit++) {
